55222bd9d292164169ba83d1552eca5e71ade719
adelaide.antunes <adelaide.antunes@squadra.com.br>
2018-01-15 09:35:29 -0200

    Incluir Recurso

---
+++ SQH.Entities/Models/Recurso/RecursoModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SQH.Entities.Models.Recurso
+{
+    public class RecursoModel
+    {
+        public int? Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O campo 'Nome' é obrigatório.")]
+        public string Nome { get; set; }
+    }
+}
