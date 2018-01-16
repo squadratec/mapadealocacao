69995b61f53e9040bc3eb570bd7492471216a959
Bruno Polito <bruno.polito@squadra.com.br>
2018-01-16 11:34:24 -0200

    Adicionado camadas ao DI

---
+++ SQH.DataAccess/Service/ProjetoRepository.cs
@@ -0,0 +1,13 @@
+﻿using SQH.DataAccess.Contract;
+using SQH.Entities.Database;
+
+namespace SQH.DataAccess.Service
+{
+    public class ProjetoRepository : DapperRepository<Projeto>, IProjetoRepository
+    {
+        public ProjetoRepository(IDatabaseConfig _config) : base(_config)
+        {
+
+        }
+    }
+}
